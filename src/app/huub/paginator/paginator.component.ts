17f3b0cf6f23af7f97b4420fd8eff0362a63d8a8
diegroxdibp <60371969+diegroxdibp@users.noreply.github.com>
2021-07-17 17:59:09 +0100

    Huub update
    search engine of huub app

--- src/app/huub/paginator/paginator.component.ts
+++ src/app/huub/paginator/paginator.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
 
 @Component({
@@ -8,49 +7,45 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./paginator.component.scss']
 })
 export class PaginatorComponent implements OnInit {
-  @Output() updatePage = new EventEmitter<any>();
-  @Input() paginator;
-  form: FormGroup;
-  pageSize = 20;
+  @Output() updatePage = new EventEmitter<object>();
+  @Input() numberOfElements: number;
   pageSizeOptions = [10, 20, 30];
-  page = 0;
-  goToProductNumer: number;
+  // form: FormGroup;
+  // goToProductNumer: number;
   constructor() {
-    this.form = new FormGroup({
-      goToProduct: new FormControl('', [Validators.min(0), Validators.max(100)]),
-      lastName: new FormControl(''),
-      age: new FormControl('')
-    });
+    // this.form = new FormGroup({
+    //   goToProduct: new FormControl('', [Validators.min(0), Validators.max(100)]),
+    //   lastName: new FormControl(''),
+    //   age: new FormControl('')
+    // });
   }
 
   ngOnInit(): void {
   }
 
+  get pageSize(): number {
+    return this.pageSizeOptions[1];
+  }
+
   onPageChange($event: PageEvent): void {
-    const pageEvent = {
-      page: $event.pageIndex + 1,
-      pageSize: $event.pageSize,
-    };
-    this.updatePage.emit(pageEvent);
+    this.updatePage.emit($event);
   }
 
-  onSubmit(): void {
+  onSubmit(): void { }
 
-  }
-
-  minMaxer(): void {
-    const inputValue = this.goToProductNumer;
-    const min = 0;
-    const max = this.paginator?.total_items_count;
-    let output: number;
-    if (inputValue < min) {
-      output = min;
-    } else if (inputValue > max) {
-      output = max;
-    } else {
-      output = inputValue;
-    }
-    this.goToProductNumer = output;
-  }
-
+  // WIP Feature
+  // minMaxer(): void {
+  //   const inputValue = this.goToProductNumer;
+  //   const min = 0;
+  //   const max = this.numberOfElements;
+  //   let output: number;
+  //   if (inputValue < min) {
+  //     output = min;
+  //   } else if (inputValue > max) {
+  //     output = max;
+  //   } else {
+  //     output = inputValue;
+  //   }
+  //   this.goToProductNumer = output;
+  // }
 }
--- src/app/huub/search/search.component.ts
+++ src/app/huub/search/search.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
-import { debounceTime, map, startWith, tap } from 'rxjs/operators';
-import { HuubServiceService } from '../huub-service.service';
+import { debounceTime, map } from 'rxjs/operators';
+import { IProduct } from '../models/IProduct';
+import { IResponse } from '../models/IResponse';
 
 @Component({
   selector: 'app-product-search',
@@ -10,23 +11,66 @@ import { HuubServiceService } from '../huub-service.service';
   styleUrls: ['./search.component.scss']
 })
 export class ProductsSearchComponent implements OnInit {
-  // @Output() updatePage = new EventEmitter<any>();
-  @Input() allProducts$;
-  searchResult;
+  // Obervable sent by the parent
+  @Input() allProducts$: Observable<IResponse>;
+  // Array of products to be used on pagination
+  arrayOfSearchedProducts: IProduct[];
+  // Array of products to be shown on view
+  searchResult: IProduct[];
+  // Starting value of paginator page size
+  pageSize = 20;
+  // Total number of products found
+  numberOfProductsFoundOnSearch: number;
+  constructor() { }
 
-  constructor(private huub: HuubServiceService) { }
-
-  ngOnInit() {
-
+  ngOnInit(): void {
   }
 
-  search($event) {
-    const searchTerm = $event.target.value;
-    const result = this.allProducts$.pipe(
-      map((response: any) => response.data),
-      map((products: any) => products.filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))),
-      debounceTime(1000),
-    ).subscribe(data => this.searchResult = data);
+  search($event: KeyboardEvent): void {
+    console.log($event);
+    const searchTerm = ($event.target as HTMLInputElement).value;
+    this.allProducts$.pipe(
+      map((response: IResponse) => response.data),
+      map((products: IProduct[]) => {
+        const arrayOfSearchedProducts = products.filter(
+          (product: IProduct) => product.name.toLowerCase().includes(searchTerm.toLowerCase().toString())
+        );
+        this.arrayOfSearchedProducts = arrayOfSearchedProducts;
+        this.numberOfProductsFoundOnSearch = arrayOfSearchedProducts.length;
+        const chunkSize = this.pageSize;
+        this.splitArrayByPageSizeAndReturnChunkCorrespondingToPageNumber(arrayOfSearchedProducts, chunkSize, this.pageSize);
+        const chunkedArraysOfSearchedProducts = this.splitArrayByPage(arrayOfSearchedProducts, chunkSize);
+        return chunkedArraysOfSearchedProducts[0];
+      }),
+      debounceTime(2000),
+    ).subscribe((response: IProduct[]) => {
+      console.log(response);
+      this.searchResult = response;
+    });
   }
 
+  splitArrayByPageSizeAndReturnChunkCorrespondingToPageNumber(array: IProduct[], chunkSize: number, page: number): IProduct[] {
+    const pageAsIndex = page - 1;
+    const splittedArray = this.splitArrayByPage(array, chunkSize);
+    const chunkReferringToPage = splittedArray[pageAsIndex];
+    return chunkReferringToPage;
+  }
+
+  // Triggered by paginator // Updates the view with the parameters sent by the paginator
+  updatePage(pageEvent: PageEvent): void {
+    const page = pageEvent.pageIndex + 1;
+    const pageSize = pageEvent.pageSize;
+    this.pageSize = pageSize;
+    this.searchResult = this.splitArrayByPageSizeAndReturnChunkCorrespondingToPageNumber(this.arrayOfSearchedProducts, pageSize, page);
+  }
+
+  splitArrayByPage(array: IProduct[], chunkSize: number): IProduct[][] {
+    const numberOfElementsInArray = array.length;
+    const chunkedArrays = [];
+    for (let i = 0; i < numberOfElementsInArray; i += chunkSize) {
+      const chunkOfArray = array.slice(i, i + chunkSize);
+      chunkedArrays.push(chunkOfArray);
+    }
+    return chunkedArrays;
+  }
 }
